3054d36f9558c28a242955e3eed3e02536565b12
Mikhail <mihailfusion@gmail.com>
2018-11-24 19:10:26 +0300

    done

---
+++ АРХИВ ДЛЯ ИНТ JS/js/script.js
@@ -0,0 +1,26 @@
+let chooseBtn = document.querySelector('#choose'),
+    receiveBtn = document.querySelector('#receive'),
+    heading = document.getElementsByTagName('h2')[0],
+    nameInput = document.getElementsByClassName('contactform_name')[0],
+    phoneInput = document.querySelector('.contactform_phone'),
+    mailInput = document.querySelectorAll('.contactform_mail')[0],
+    modal = document.querySelector('.modal'),
+    close = document.querySelector('.close'),
+    text = document.getElementsByName('message')[0];
+
+
+
+receiveBtn.addEventListener('click', function () {
+  modal.style.display = "block"
+});
+close.addEventListener('click', function () {
+  modal.style.display = "none"
+});
+
+nameInput.addEventListener("input", function() { 
+  text.value = "Меня зовут " + nameInput.value + ". И я хочу у вас спросить.";
+
+  if (nameInput.value == "") {
+    text.value = ""
+  }
+ });
